97c17997fe16c16e355ad5979e506f66018ffb14
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-09-20 19:15:19 +0200

    Implement update score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -4,10 +4,25 @@ class ScoresController < ApplicationController
     render json: scores
   end
 
+  def show
+    score = Score.find(params[:id])
+    render json: score
+  end
+
   def create
     score = Score.create(score_params)
     render json: score, status: :created
   end
+
+  def update
+    score = Score.find(params[:id])
+    if score.update(score_params)
+      render json: score, status: :ok
+    else
+      render json: score.errors, status: :uprocessable_entity
+    end
+  end
+
 
   private
 
