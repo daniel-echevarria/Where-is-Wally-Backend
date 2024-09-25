c512cd4662a44287fe30db289e1fb44797ef651c
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-09-25 17:00:00 +0200

    Add configuration to allow cors from front end in both environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
   # allowed requests from frontend app
   config.allowed_origin = 'https://wherethefuckiswaldo.netlify.app'
 end
