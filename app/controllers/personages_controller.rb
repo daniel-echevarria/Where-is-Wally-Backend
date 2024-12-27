7d165ec044cdb98d9787f16d78a10ee12ae5cab2
Kasper Bak <dan.echevarri@gmail.com>
2024-12-27 16:07:05 +0100

    Move logic for confirming coordinates from frontend to backend

--- app/controllers/personages_controller.rb
+++ app/controllers/personages_controller.rb
@@ -1,6 +1,25 @@
 class PersonagesController < ApplicationController
   def index
     personages = Personage.all
-    render(json: personages)
+    personages_names = personages.map(&:name)
+    render(json: personages_names)
+  end
+
+  def check
+    params = check_params
+    personage = Personage.find_by(name: params[:name])
+    puts "this is the personage #{personage}"
+    puts "this is the x coordinates in the database #{personage[:x]}"
+    puts "this is the coordinates #{params[:coordinates]}"
+    puts "this is circle_radius #{params[:circle_radius]}"
+    answer = personage.in_circle?(params[:coordinates], params[:circle_radius])
+    puts "this is the answer #{answer}"
+    render(json: answer)
+  end
+
+  private
+
+  def check_params
+    params.permit('name', 'circle_radius', coordinates: {})
   end
 end
--- app/models/personage.rb
+++ app/models/personage.rb
@@ -1,2 +1,15 @@
 class Personage < ApplicationRecord
+  def in_range?(value, target, error_margin)
+    puts " #{value}, #{target}, #{error_margin}"
+    min = target - error_margin
+    max = target + error_margin
+    value.between?(min, max)
+  end
+
+  def in_circle?(coordinates, circle_radius)
+    puts "this is the coordinates inside the method #{coordinates}"
+    puts "this is the circle_radius inside the method #{circle_radius}"
+    in_range?(x, coordinates[:x], circle_radius) &&
+      in_range?(y, coordinates[:y], circle_radius)
+  end
 end
